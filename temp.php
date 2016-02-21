6710f887ffae2e4b9acf2e759ea9a9ccb5a32ea8
VistaarJ <vistaarjuneja@yahoo.com>
2016-02-22 01:31:21 +0530

    API Working to get medicine details

--- temp.php
+++ temp.php
@@ -1,27 +1,38 @@
 <?php
-echo "Hi!";
+
 
 if(isset($_GET['query']))
 {
-	echo "Hi!";
+	
 	if(!empty($_GET['query']))
 	{
 		$que = $_GET['query'];
 		$url = 'https://api.fda.gov/drug/label.json?search=' . urlencode($que);
-		echo $url;
-		$curlSession = curl_init($url);
-    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-   $data = curl_exec($curlSession);
-    
-    curl_close($curlSession);
-    var_dump(json_decode($data));
-var_dump(json_decode($data, true));
-  //   $url_array = json_decode($data);
-		// $info = $url_array['results'][0]['indications_and_usage']; 
-		// echo "Hello!";
-		// echo $info;
+	
+	$proxy = '172.16.115.19:3128';
+//$proxyauth = 'user:password';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$data = curl_exec($ch);
+
+curl_close($ch);
+	  // $result = json_decode($data, true);
+$result = json_decode($data, true);
+$result_string = $result['results'][0]['indications_and_usage'][0];
+
+echo $result_string;
+//print_r($result->results[0]->indications_and_usage);
+	//$result_string = $result->$result['results'][0]['indications_and_usage'][0];
+	//echo $result_string;
 	}
 }
+
 
 ?>
 
