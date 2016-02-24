d34e6c2c04dfe2b7f0e2284a5aed729d1453da5b
tyagiabhishek13 <tyagiabhishek13@gmail.com>
2016-02-24 22:25:41 +0530

    Front end of Diagnosis (ADD this one)

--- diagnosis.php
+++ diagnosis.php
@@ -12,14 +12,13 @@ if(!isset($_SESSION['id']))
          $query2 = "SELECT * from patients WHERE roll='$id'";
                         $run1 = mysqli_query($conn,$query2);
                         $row2=mysqli_fetch_assoc($run1);
-                        echo $row2["name"];
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Home Page - Receptionist</title>
+    <title>Diagnosis Page</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
@@ -31,42 +30,68 @@ if(!isset($_SESSION['id']))
     <script src="AdminLTE/js/app.js"></script>
 </head>
 <body>
-<div class="row">
-    <nav class="navbar navbar-inverse navbar-fixed-top" style="height: 10%">
+<div class="row" style="padding: 0px">
+    <nav class="navbar navbar-inverse navbar-fixed-top" style="padding: 0px; height: 10%">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="staff_recep.php" style="font-size: xx-large"><b>HOSPITAL - Indian Institute of Technology Guwahati</b></a>
+                <a class="navbar-brand" href="staff_recep.php" style="font-size: xx-large; padding: 0px"><b>HOSPITAL - Indian Institute of Technology Guwahati</b></a>
             </div>
             <ul class="nav navbar-nav navbar-right">
-                <li><a class="navbar-brand" href="logout.php?logout" style="font-size: large"><b>Logout</b></a></li>
+                <li><a class="navbar-brand" href="logout.php?logout" style="font-size: large ; padding: 0px"><b>Logout</b></a></li>
             </ul>
         </div>
     </nav>
 </div>
-<div class="small-box bg-yellow">
+<div class="small-box bg-yellow" style="padding: 0px">
             <div class="inner">
-              <h3><?php echo ("Patient id : ".$_SESSION['patient_id']);?></h3>
+              <h4><?php echo ("Patient id :  ".$_SESSION['patient_id']);?></h4>
+              <h4><?php echo ("Name :   ".$row2["name"]);?></h4>
 
-              <p><?php echo ("Name : ". $row2["name"]);?></p>
+              <h5><?php echo ("Gender : ". $row2["gender"]);?></h5>
+              <h5><?php echo ("Date of Birth : ". $row2["dob"]);?></h5>
+              <p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
             <a href="#" class="small-box-footer">
-              Medical info <i class="fa fa-arrow-circle-right"></i>
+              Medical History <i class="fa fa-arrow-circle-right"></i>
             </a>
           </div>
 
-          <div class="small-box bg-yellow" style="padding: 0px">
-            <div class="inner">
-              <h3><?php echo ("Patient id : ".$_SESSION['patient_id']);?></h3>
 
-              <h3><?php echo ("Name : ". $row2["name"]);?></h3>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-              Medical info <i class="fa fa-arrow-circle-right"></i>
-            </a>
-          </div>
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+ <label for="Diagnosis">Diagnosis</label>
+<textarea class="form-control" rows="5" placeholder="Rx." id = "Diagnosis"></textarea>
+
+
+<div class="field_wrapper">
+    <div>
+    <label for="Medicine">Medicines: </label>
+        <input class="form-control" placeholder="Prescription" type="text">
+        <span class="input-group-btn">
+                      
+                    </span>
+        <a href="javascript:void(0);" class="add_button" title="Add field"><img src="add-icon.png" height="50" width="50" /></a>
+    </div>
+</div>
+<script type="text/javascript">
+$(document).ready(function(){
+    var max = 25; 
+    var addButton = $('.add_button'); 
+    var fwrapper = $('.field_wrapper'); 
+    var fieldHTML = '<div> <input class="form-control" placeholder="Prescription" type="text"><a href="javascript:void(0);" class="remove_button" title="Remove field"><img src="remove-icon.png" height="30" width="30"/></a></div>'; //New input field html for when add button is clicked
+    var x = 1; //Initial field counter is 1
+    $(addButton).click(function(){ //Once add button is clicked
+        if(x < max){ //Check maximum number of input fields if not exceeded
+            x++; //Increment field counter
+            $(fwrapper).append(fieldHTML); // Add field html
+        }
+    });
+    $(fwrapper).on('click', '.remove_button', function(e){ //Once remove button is clicked
+        e.preventDefault();
+        $(this).parent('div').remove(); //Remove field html
+        x--; //Decrement field counter
+    });
+});
+</script>
