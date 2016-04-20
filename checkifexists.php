40c46dbc55f263846c702ce995bd7cb82d070630
VistaarJ <vistaarjuneja@yahoo.com>
2016-04-20 16:27:52 +0530

    created table and sends sql queries to check availability

--- checkifexists.php
+++ checkifexists.php
@@ -1,7 +1,28 @@
 <?php
+session_start();
+include_once 'dbconnect.php';
 
-$name_of_medicine = $_REQUEST["medname"];
-$quantity_of_medicine = $_REQUEST["quantity"];
-//check if it is valid and echo appropriate response 
-echo $name_of_medicine . "<br>" . $quantity_of_medicine;
+$name_of_medicine = mysqli_real_escape_string($conn,$_REQUEST["medname"]);
+$quantity_of_medicine = mysqli_real_escape_string($conn,$_REQUEST["quantity"]);
+
+$slquery = "SELECT * FROM inventory WHERE mname = '$name_of_medicine'";
+$selectresult = mysqli_query($conn,$slquery);
+$row_cnt = mysqli_num_rows($selectresult);
+$arr = mysqli_fetch_assoc($selectresult);
+$available_quantity = $arr["mquantity"];
+if($row_cnt==0)
+{
+	echo "DOES NOT EXIST";
+}
+else
+{
+	if($available_quantity<$quantity_of_medicine)
+	{
+		echo "Less quantity!";
+	}
+	else
+	{
+		echo "Available!";
+	}
+}
 ?>
